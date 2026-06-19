e3109011554eb9e65c51e73e79c0752ea069a036
kaswanhitesh <hiteshkaswan@icloud.com>
2026-06-20 03:17:28 +0530

    Fix HTML structure in Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -144,10 +144,13 @@ export default function Header() {
                   Transshipment
                  </a>
 
-                <p className="py-3 px-16">
+                <a
+                  href="/services/transport-engineering"
+                  className="block py-3 px-16 hover:bg-white hover:text-gray-700 transition-all duration-300"
+                 >
                   Transport Engineering Capabilities
-                </p>
-
+                 </a>
+                
                 <a
                   href="/services/ocean-freight"
                   className="block py-3 px-16 hover:bg-white hover:text-gray-700 transition-all duration-300"
