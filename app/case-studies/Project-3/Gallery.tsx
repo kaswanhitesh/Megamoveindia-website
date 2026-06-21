a91bcc823fe6ab80a792ca3b417ed1460b61c337
kaswanhitesh <hiteshkaswan@icloud.com>
2026-06-22 01:55:39 +0530

    Update image paths in Gallery component

--- app/case-studies/Project-3/Gallery.tsx
+++ app/case-studies/Project-3/Gallery.tsx
@@ -4,18 +4,14 @@ import { useState } from "react";
 
 export default function Gallery() {
   const images = [
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery1.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery2.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery3.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery4.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery5.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery6.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery7.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery8.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery9.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery10.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery11.webp",
-    "/images/Casestudies/DefenceCargo/defencecargo_Gallery12.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery1.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery2.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery3.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery4.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery5.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery6.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery7.webp",
+    "/images/Casestudies/Project-3/Project3_Gallery8.webp",
   ];
 
   const [selectedImage, setSelectedImage] = useState<number | null>(null);
